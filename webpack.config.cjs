a0be1f4fd3f5ce6b9a279e1477a759e86a0110e0
programacionparaaprender <yancel209@gmail.com>
2025-12-07 18:48:58 -0500

    agregando cambios

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "https://fantastic-mf-colorpicker-host.netlify.app/",
+    publicPath: "https://mf-colorpicker-host-ts.netlify.app/",
     //publicPath: "http://localhost:3000/",
   },
 
